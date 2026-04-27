16e7941ff91565dd960c41812df5c6445a4e5041
v.novoselov <v.novoselov@strikerz.inc>
2026-04-28 03:18:24 +0500

    Rework project's architecture!

---
+++ engine/source/Engine.cpp
@@ -0,0 +1,52 @@
+﻿#include "Engine.h"
+#include "Application.h"
+
+namespace eng
+{
+    bool Engine::Init()
+    {
+        if (!m_application)
+        {
+            return false;
+        }
+        
+        return m_application->Init();
+    }
+
+    void Engine::Run()
+    {
+        if (!m_application)
+        {
+            return;
+        }
+        
+        m_lastTimeTime = std::chrono::high_resolution_clock::now();
+        while (!m_application->NeedsToBeClosed())
+        {
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            float DeltaTime = std::chrono::duration<float>(currentTime - m_lastTimeTime).count();
+            m_lastTimeTime = currentTime;
+            
+            m_application->Update(DeltaTime);
+        }
+    }
+
+    void Engine::Destroy()
+    {
+        if (m_application)
+        {
+            m_application->Destroy();
+            m_application.reset();
+        }
+    }
+
+    void Engine::SetApplication(Application* app)
+    {
+        m_application.reset(app);
+    }
+
+    Application* Engine::GetApplication()
+    {
+        return m_application.get();
+    }
+}
---
+++ engine/source/Engine.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "memory"
+#include "chrono"
+
+namespace eng
+{
+    class Application;
+    class Engine
+    {
+    public:
+        bool Init();
+        void Run();
+        void Destroy();
+        
+        void SetApplication(Application* app);
+        Application* GetApplication();
+        
+     private:
+        std::unique_ptr<Application> m_application;
+        std::chrono::high_resolution_clock::time_point m_lastTimeTime;
+    };   
+}
